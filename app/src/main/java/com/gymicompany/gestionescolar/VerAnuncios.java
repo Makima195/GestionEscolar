e3c7c7c01db3e20b7dbec0a69532dfa7ca24be17
Raitogure <vegayehoshua@gmail.com>
2023-12-06 22:02:35 -0300

    commit

--- app/src/main/java/com/gymicompany/gestionescolar/VerAnuncios.java
+++ app/src/main/java/com/gymicompany/gestionescolar/VerAnuncios.java
@@ -1,103 +1,62 @@
 package com.gymicompany.gestionescolar;
 
-import androidx.annotation.NonNull;
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+
+import io.reactivex.annotations.NonNull;
+
 public class VerAnuncios extends AppCompatActivity {
-    TextView Anuncio;
-    TextView Titulo;
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-    CollectionReference collectionRef = db.collection("anuncios");
-
-
-    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_anuncios);
+        // Inicializar Firestore
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        Anuncio = findViewById(R.id.Anuncio_ver);
-        Titulo = findViewById(R.id.Titulo_ver);
+        // Obtener referencia al ListView en tu diseño
+        ListView listView = findViewById(R.id.lista_anuncios);
 
-        user = FirebaseAuth.getInstance().getCurrentUser();
+        // Crear una lista para almacenar los datos de Firestore
+        final ArrayList<String> dataList = new ArrayList<>();
 
-        //CargarAnuncio();
-        Prueba();
-
-    }
-
-    public void Prueba(){
-
-        collectionRef.orderBy("anuncios", Query.Direction.DESCENDING)
-                .limit(1)
+        // Obtener datos de Firestore y actualizar el ListView
+        db.collection("anuncios")
                 .get()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        if (!queryDocumentSnapshots.isEmpty()) {
-                            DocumentSnapshot document = queryDocumentSnapshots.getDocuments().get(0);
-                            /// Accede a los campos que deseas mostrar en los TextViews
-                            String campo1 = document.getString("Anuncio");
-                            String campo2 = document.getString("Titulo");
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                // Aquí puedes acceder a los datos de cada documento
+                                String titulo = document.getString("Titulo");
+                                String anuncio = document.getString("Anuncio");
+                                // Agregar ambos datos a la lista
+                                dataList.add(titulo + ": " + anuncio);
+                            }
 
-                            // Actualiza los TextViews con los valores de los campos
-                            Anuncio.setText(campo1);
-                            Titulo.setText(campo2);
+                            // Actualizar el ListView con los datos
+                            ArrayAdapter<String> adapter = new ArrayAdapter<>(VerAnuncios.this,
+                                    android.R.layout.simple_list_item_1, dataList);
+                            listView.setAdapter(adapter);
                         } else {
-                            // La colección está vacía, no hay documentos
-                            Log.d("TAG", "La colección está vacía");
+                            Log.e(TAG, "Error obteniendo documentos: ", task.getException());
                         }
                     }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Manejar errores de lectura
-                        Log.e("TAG", "Error al leer el documento", e);
-                    }
                 });
     }
-
-
-     /* private void CargarAnuncio() {
-        db.collection("user")
-                .document(user.getUid())
-                .get()
-                .addOnSuccessListener(documentSnapshot -> {
-                    if (documentSnapshot.exists()) {
-                        // RECUERDA LA PUTA MAYUSCULA DE "C"URSO >:DDDDDDD
-                        String pruebacurso = documentSnapshot.getString("anuncio_general");
-                        db.collection("anuncios")
-                                .document(pruebacurso)
-                                .get()
-                                .addOnSuccessListener(documentSnapshotanuncios -> {
-                                    if (documentSnapshotanuncios.exists()) {
-                                        Titulo.setText(documentSnapshotanuncios.getString("Titulo"));
-                                        Anuncio.setText(documentSnapshotanuncios.getString("Anuncio"));
-                                        //EJEMPLO DE OTROS DIAS TODO PUEDE ESTAR JUNTO
-                                        //martesbloque2.setText(documentSnapshotHorario.getString("bloque2"));
-                                        // Puedes continuar con el resto de los bloques o datos aquí.
-                                    }
-                                });
-                    }
-                });
-    }
-    */
-
 }
